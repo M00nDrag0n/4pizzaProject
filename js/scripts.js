ef6404ad08201b014041504333d501c69715b67c
Student <example@example.com>
2019-11-08 10:19:09 -0800

    apply JavaScript in a weird order to my scripts.js file so I can modify it in a minute

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,27 @@
  }
 
 
+Pizza.prototype.calculate = function () {
+  this.toppings.forEach(function(oneTopping) {
+    this.price +=1;
+  })
 
-
-
-
-
+  if(this.size == "small") {
+    this.price += 4;
+  } else {
+    this.price += 20;
+  }
+  return this.price;
+};
 
 $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
+
+    let 
+
+
+
+
   });
 });
