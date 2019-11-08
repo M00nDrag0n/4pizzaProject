6d3a5deaaab870e5b5df6ae72e905fd6062ec700
Student <example@example.com>
2019-11-08 09:55:14 -0800

    remember to make a commit, Anise

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+ function Pizza(toppings, size) {
+   this.toppings = [],
+   this.size = size,
+   price = 0,
+ }
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $("#input").submit(function(event) {
+    event.preventDefault();
+  });
+});
